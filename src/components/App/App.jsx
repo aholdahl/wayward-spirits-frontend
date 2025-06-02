e56d14f5c219ae8c118f28326f9281ebe60f1616
Allyson Holdahl <allyson.holdahl@gmail.com>
2025-06-02 18:53:44 -0500

    add header footer and nav

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import Header from '../Header/Header';
+import Nav from '../Nav/Nav';
+import Footer from '../Footer/Footer';
 
 export default function App () {
     return (
-        <h1>Hello world</h1>
+        <>
+        <Header />
+        <Nav />
+        <Footer />
+        </>
     )
 }
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Header() {
+    return (
+        <header>
+            <h1>Wayward Spirits</h1>
+            <h2>A practical approach to mixology</h2>
+        </header>
+    )
+}
